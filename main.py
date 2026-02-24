dd23e930c07d9b5ed64dee5aa57559bdd6c129fb
erik73491020 <erik7349@gmail.com>
2026-02-24 23:38:37 +0000

    Revert feature-2 merge – removed feature 2 changes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 print("Task 3 – simulated clone test")
 print("Task 3 – testing changes in my assignment repo")
 print("Task 3 – change pushed back to repo")
-print("Feature 2 changes")
 # Tag v1.0 – First stable version after merging feature branches
